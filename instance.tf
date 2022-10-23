9109cc7e21890b845e91a31cf8e9d3c6d8d08b28
Mohamed Ali <71033834+moe-Ali@users.noreply.github.com>
2022-10-23 17:10:27 +0200

    Add files via upload

--- instance.tf
+++ instance.tf
@@ -2,6 +2,8 @@ resource "aws_instance" "public1" {
   ami           = var.ami
   instance_type = var.instance_type
   subnet_id     = module.network.public1_id
+  vpc_security_group_ids = aws_security_group.ec2_sg.id
+
   tags = {
     Name = "ter_ec2_1"
   }
@@ -11,6 +13,8 @@ resource "aws_instance" "public2" {
   ami           = var.ami
   instance_type = var.instance_type
   subnet_id     = module.network.public2_id
+  vpc_security_group_ids = aws_security_group.ec2_sg.id
+
   tags = {
     Name = "ter_ec2_2"
   }
---
+++ security_group.tf
@@ -0,0 +1,38 @@
+resource "aws_security_group" "ec2_sg" {
+  name        = "ec2_sg"
+  description = "Allow HTTP(s) and SSH trafic to ec2"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "ec2_sg"
+  }
+}
